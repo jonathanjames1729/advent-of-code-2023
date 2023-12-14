375b9d8473fd07b33168c395bc927e60bb904f2f
Jonathan James <jonathan.james@fundingcircle.com>
2023-12-14 01:37:09 +0000

    Day 13, part 2

--- src/day13.rb
+++ src/day13.rb
@@ -5,8 +5,16 @@ require_relative 'base'
 class Day13 < Base
   def part1
     data.map do |lines|
-      vertical = find_vertical_reflection(lines)
-      horizontal = find_horizontal_reflection(lines)
+      vertical = find_vertical_reflection(lines) { |l| find_reflection_from_top(l) }
+      horizontal = find_horizontal_reflection(lines) { |l| find_reflection_from_top(l) }
+      (vertical.nil? ? 0 : 100 * vertical) + (horizontal.nil? ? 0 : horizontal)
+    end.sum
+  end
+
+  def part2
+    data.map do |lines|
+      vertical = find_vertical_reflection(lines) { |l| find_reflection_with_smudge_from_top(l) }
+      horizontal = find_horizontal_reflection(lines) { |l| find_reflection_with_smudge_from_top(l) }
       (vertical.nil? ? 0 : 100 * vertical) + (horizontal.nil? ? 0 : horizontal)
     end.sum
   end
@@ -34,16 +42,31 @@ class Day13 < Base
     nil
   end
 
-  def find_vertical_reflection(lines)
-    axis = find_reflection_from_top(lines)
+  def measure_string_difference(one, two)
+    (0...one.length).count { |i| one[i] != two[i] }
+  end
+
+  def find_reflection_with_smudge_from_top(lines)
+    (1...lines.count).step(2) do |i|
+      differences = (0..(i / 2)).filter { |j| lines[j] != lines[i - j] }
+      next unless differences.count == 1
+
+      j = differences.first
+      return (i + 1) / 2 if measure_string_difference(lines[j], lines[i - j]) == 1
+    end
+    nil
+  end
+
+  def find_vertical_reflection(lines, &blk)
+    axis = blk.call(lines)
     return axis unless axis.nil?
 
-    axis = find_reflection_from_top(lines.reverse)
+    axis = blk.call(lines.reverse)
     (lines.count - axis) unless axis.nil?
   end
 
-  def find_horizontal_reflection(lines)
-    find_vertical_reflection(transpose(lines))
+  def find_horizontal_reflection(lines, &)
+    find_vertical_reflection(transpose(lines), &)
   end
 end
 
